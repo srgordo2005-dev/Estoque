01c20af033211652af73c814eb8538898aa1dacf
FELIPE <SRGORDO2005@GMAIL.COM>
2026-07-06 11:18:52 -0300

    Importando hooks do react que faltavam

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import { jsPDF } from 'jspdf';
 import { BrowserMultiFormatReader } from '@zxing/browser';
